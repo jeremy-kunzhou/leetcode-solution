e81772fa31053fe2a02d7221af588a0b4797882a
Jeremy Zhou <jeremy.kunzhou@gmail.com>
2024-07-03 17:11:47 +1000

    add template for KMP

---
+++ String/Template/KMP.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  if (needle.length == 0) return 0;
+  const lps = Array(needle.length).fill(0)
+  let prevLPS = 0, i = 1;
+  while (i < needle.length) {
+    if (needle[i] == needle[prevLPS]) {
+      lps[i] = prevLPS + 1;
+      prevLPS++;
+      i++
+    } else if (prevLPS == 0) {
+      lps[i] = 0;
+      i++
+    } else {
+      prevLPS = lps[prevLPS - 1]
+    }
+  }
+
+  let hP = 0;
+  let nP = 0;
+  while (hP < haystack.length) {
+    if (haystack[hP] == needle[nP]) {
+      hP++;
+      nP++;
+    } else {
+      if (nP == 0) { hP++ }
+      else { nP = lps[nP - 1] }
+    }
+    if (nP == needle.length) {
+      return hP - needle.length
+    }
+  }
+  return -1
+};
