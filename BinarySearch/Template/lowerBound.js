f4e04e369fecd5947a0986ed7a6ff51eebfbe8ed
Jeremy Zhou <jeremy.kunzhou@gmail.com>
2024-09-08 16:34:38 +1000

    add template lastBound

--- BinarySearch/Template/lowerBound.js
+++ BinarySearch/Template/lowerBound.js
@@ -4,7 +4,7 @@ function lowerBound(nums, target) {
   let left = 0, right = nums.length;
 
   while (left < right) {
-    const mid = left + (right - left) / 2;
+    const mid = Math.floor((right + left) / 2);
     if (valid(nums[mid], target)) {
       // Move the right pointer if the mid value is valid
       right = mid;
@@ -18,4 +18,21 @@ function lowerBound(nums, target) {
 function valid(num, target) {
   // Check if the current number meets the target condition
   return num >= target;
+}
+
+// Finds the index of the last element in a sorted list that is
+// greater than or equal to a specified target value using binary search.
+function lastBound(nums, target) {
+  let left = 0, right = nums.length;
+
+  while (left < right) {
+    const mid = Math.ceil((right + left) / 2);
+    if (valid(nums[mid], target)) {
+      // Move the right pointer if the mid value is valid
+      left = mid;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return right;
 }
